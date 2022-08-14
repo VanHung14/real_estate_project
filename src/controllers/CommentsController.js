6498a71bd7f2102b229ebcf0b1ac53f83c795a58
VanHung14 <dinhvanhung173@gmail.com>
2022-08-15 00:34:12 +0700

    refactor: code CommentsController #1, transaction

--- src/controllers/CommentsController.js
+++ src/controllers/CommentsController.js
@@ -1,5 +1,7 @@
 const { PrismaClient } = require("@prisma/client");
+const variable = require("../configs/variable");
 const prisma = new PrismaClient();
+const CommentsService = require("../services/CommentsService");
 
 class CommentsController {
   // [POST] /api/comments/
@@ -8,65 +10,47 @@ class CommentsController {
       let postId = parseInt(req.body.post_id);
       let date = new Date();
       date.setHours(date.getHours() + 7);
-      let comment = await prisma.comments.create({
-        data: {
-          user_id: req.user.id,
-          post_id: postId,
-          comment: req.body.comment,
-          created_at: date,
-          updated_at: date,
-        },
-      });
-      if (comment) {
-        res.status(200).send(comment);
-      } else {
-        res.status(400).send("Comment failed!");
-      }
+      let data = {
+        user_id: req.user.id,
+        post_id: postId,
+        comment: req.body.comment,
+        created_at: date,
+        updated_at: date,
+      };
+      let createCmt = await CommentsService.createComment(data);
+      if (createCmt == variable.BadRequest)
+        return res.status(variable.BadRequest).send("Create comment failed!");
+      return res.send(createCmt);
     } catch (err) {
-      res.status(400).send(err);
+      res.status(variable.BadRequest).send(err.message);
     }
   }
 
   // [PATCH] /api/comments/:id
-  // Only works with users who own this comment
   async updateComment(req, res, next) {
     try {
       let date = new Date();
       date.setHours(date.getHours() + 7);
+      let data = {
+        comment: req.body.comment,
+        updated_at: date,
+      };
+      let user_id = req.user.id;
       let id = parseInt(req.params.id);
-      let comment = await prisma.comments.findFirst({ where: { id: id } });
-      if (comment) {
-        if (req.user.id == comment.user_id) {
-          let update = await prisma.comments.update({
-            where: {
-              id: id,
-            },
-            data: {
-              comment: req.body.comment,
-              updated_at: date,
-            },
-          });
-          if (update) {
-            res.send(update);
-          } else {
-            res.status(400).send("Update comment failed!");
-          }
-        } else {
-          res
-            .status(403)
-            .send("Not permission! Only works with users who own this comment");
-        }
-      } else {
-        res.status(204).send("No comment found!");
-      }
+      let updateCmt = await CommentsService.updateComment(id, data, user_id);
+      if (updateCmt == variable.NoContent)
+        return res.status(variable.NoContent).send();
+      if (updateCmt == variable.BadRequest)
+        return res.status(variable.BadRequest).send("Update failed!");
+      if (updateCmt == variable.Forbidden)
+        return res.status(variable.Forbidden).send("No permission!");
+      return res.send(updateCmt);
     } catch (err) {
-      res.status(400).send(err);
+      res.status(variable.BadRequest).send(err.message);
     }
   }
 
   // [GET] /api/comments?page=&postId=
-  // GET all comments (have pagination), only works for admin
-  // GET comment by post_id works with any roles
   async getComments(req, res, next) {
     try {
       let postId = parseInt(req.query.postId);
@@ -76,56 +60,38 @@ class CommentsController {
         skip: perPage * page - perPage,
         take: perPage,
       };
-      if (Number.isInteger(postId)) {
+      let hasGetAllComments = !Number.isInteger(postId);
+      if (hasGetAllComments) {
+        if (req.user.role_id != 1)
+          return res.status(403).send("No permission!");
+      } else {
         data["where"] = { post_id: postId };
       }
-      console.log("data", data);
-      let comments = await prisma.comments.findMany(data);
-      // console.log(comments)
-      if (Number.isInteger(postId)) {
-        if (JSON.stringify(comments) != JSON.stringify([])) {
-          res.send(comments);
-        } else {
-          res.status(204).send("No comments found!");
-        }
-      } else {
-        if (req.user.role_id == 1 && comments) {
-          res.send(comments);
-        } else {
-          res.status(403).send("No permission! Only admin can get comments.");
-        }
-      }
+      let comments = await CommentsService.getComment(data);
+      if (comments == variable.NoContent)
+        return res.status(variable.BadRequest).send("Get all comments failed!");
+      res.send(comments);
     } catch (err) {
-      res.status(400).send(err);
+      res.status(variable.BadRequest).send(err.message);
     }
   }
 
   // [DELETE] /api/comments/:id
-  // Only works with users who own this comment, or admin
   async deleteComment(req, res, next) {
     try {
       let id = parseInt(req.params.id);
-      let comment = await prisma.comments.findFirst({ where: { id: id } });
-      if (comment) {
-        if (req.user.id == comment.user_id || req.user.role_id == 1) {
-          let delCmt = await prisma.comments.delete({ where: { id: id } });
-          if (delCmt) {
-            res.send(delCmt);
-          } else {
-            res.status(204).send("Delete comment failed!");
-          }
-        } else {
-          res
-            .status(403)
-            .send(
-              "Not permission! Only works with users who own this comment, or admin"
-            );
-        }
-      } else {
-        res.status(204).send("No comment found");
-      }
+      let user_id = req.user.id;
+      let role_id = req.user.role_id;
+      let delCmt = await CommentsService.deleteComment(id, user_id, role_id);
+      if (delCmt == variable.NoContent)
+        return res.status(variable.NoContent).send();
+      if (delCmt == variable.Forbidden)
+        return res.status(variable.Forbidden).send("No permission!");
+      if (delCmt == variable.BadRequest)
+        return res.status(variable.BadRequest).send("Delete comment failed!");
+      res.send(delCmt);
     } catch (err) {
-      res.status(400).send(err);
+      res.status(variable.BadRequest).send(err.message);
     }
   }
 }
